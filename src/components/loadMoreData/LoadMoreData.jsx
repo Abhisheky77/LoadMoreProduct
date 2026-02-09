2b05e9cdb0302f8cf0f1d0c7ce5c66097d940978
Abhisheky77 <123infinity890@gmail.com>
2026-02-09 21:32:29 +0530

    LoadMoreData

--- src/components/loadMoreData/LoadMoreData.jsx
+++ src/components/loadMoreData/LoadMoreData.jsx
@@ -15,6 +15,7 @@ function LoadMoreData() {
     const [searchlength, setSearchlength] = useState(false);
     const [filterProducts, setfilterProducts] = useState(products);
     const [scrollPercentage, setScrollPercentage] = useState(0)
+console.log(searchlength);
 
 
     async function fetchProducts() {
